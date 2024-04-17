565403961a0b7ce8d8e07ca6cfbf8dc3041fedc8
mariamfadel27 <mariamwaleed2510@gmail.com>
2024-04-17 20:31:02 +0200

    bug UI_TEST REPORT

--- UI_Testing/tests/SubmitForm_Test.js
+++ UI_Testing/tests/SubmitForm_Test.js
@@ -9,113 +9,132 @@ const path = require('path');
 // Example file name
 
 module.exports = {
-    "My first test case all enteries are given happy scenario "(browser) {
-    
-        page
-          .navigate()
-          .waitForElementVisible("body")
-          .selectFilter("@subjectHeadingDropDownSelector", CustomerService)
-          .pause(2000)
-          .Set_Value("@emailSelector", "test@gmail.com")
-          .pause(2000)
-          .Set_Value("@messageSelector", "Test Case_1")
-          .pause(2000)
-          .Set_Value("@OrderReferenceInput",Order_num)
-          .pause(2000)
-          .click("@submitButtonSelector")
-          .pause(2000)
-          .assert.visible("@successMessageSelector");
-      },
-    /** 
-     * my second test case:submit file
-    */
-      "My second test case-submit file"(browser) {
-    
-        page.click("@ContuctUs_Selector")//again t o submit another form,only first one doesn't need this
-          .waitForElementVisible("body")
-          .selectFilter("@subjectHeadingDropDownSelector", WebMaster)
-          .pause(2000)
-          .Set_Value("@emailSelector", "test@gmail.com")
-          .pause(2000)
-          .Submit_File() /*submit file*/ 
-          // Wait for the filename to be visible in the specified element
-          .Set_Value("@messageSelector", "Test Case_2: file submitted successfully")
-          .pause(2000)
-          .Set_Value("@OrderReferenceInput",Order_num)
-          .pause(2000)
-          .click("@submitButtonSelector")
-          .pause(2000)
-          .assert.visible("@successMessageSelector");
-        
-      },
-      /** 
-     * my third test case: email is missing expected to message won't send
-    */
-      "My third test case-No email"(browser) {
-    
-          page.click("@ContuctUs_Selector")//again t o submit another form,only first one doesn't need this
-            .waitForElementVisible("body")
-            .selectFilter("@subjectHeadingDropDownSelector", WebMaster)
-            .pause(2000)
-            .Set_Value("@messageSelector", "Test Case_3: failed to send this message,email is missed")
-            .pause(2000)
-            .click("@submitButtonSelector")
-            .pause(2000)
-            .assert.visible("@invalidEmailAddress");
-        },
 
-        /** 
-     * my fourth test case: subject is missing expected to message won't send
-    */
-    "My fourth test case-No subject header is selected "(browser) {
-        
+  "My first test case all enteries are given happy scenario "(browser) {
+    
+    page
+      .navigate()
+      .waitForElementVisible("body")
+      .selectFilter("@subjectHeadingDropDownSelector", CustomerService)
+      .pause(3000)
+      .Set_Value("@emailSelector", "test@gmail.com")
+      .pause(3000)
+      .Set_Value("@messageSelector", "Test Case_1")
+      .pause(3000)
+      .Set_Value("@OrderReferenceInput",Order_num)
+      .pause(3000)
+      .click("@submitButtonSelector")
+      .pause(3000)
+      .assert.visible("@successMessageSelector");
+  },
+/** 
+ * my second test case:submit file
+*/
+  "My second test case-submit file"(browser) {
+
+    page.click("@ContuctUs_Selector")//again t o submit another form,only first one doesn't need this
+      .waitForElementVisible("body")
+      .selectFilter("@subjectHeadingDropDownSelector", WebMaster)
+      .pause(2000)
+      .Set_Value("@emailSelector", "test@gmail.com")
+      .pause(3000)
+      .Submit_File() /*submit file*/ 
+      // Wait for the filename to be visible in the specified element
+      .Set_Value("@messageSelector", "Test Case_2: file submitted successfully")
+      .pause(3000)
+      .Set_Value("@OrderReferenceInput",Order_num)
+      .pause(3000)
+      .click("@submitButtonSelector")
+      .pause(3000)
+      .assert.visible("@successMessageSelector");
+    
+  },
+  /** 
+ * my third test case: email is missing expected to message won't send
+*/
+  "My third test case-No email"(browser) {
+
+      page.click("@ContuctUs_Selector")//again t o submit another form,only first one doesn't need this
+        .waitForElementVisible("body")
+        .selectFilter("@subjectHeadingDropDownSelector", WebMaster)
+        .pause(3000)
+        .Set_Value("@messageSelector", "Test Case_3: failed to send this message,email is missed")
+        .pause(3000)
+        .click("@submitButtonSelector")
+        .pause(3000)
+        .assert.visible("@invalidEmailAddress");
+    },
+
+    /** 
+ * my fourth test case: subject is missing expected to message won't send
+*/
+"My fourth test case-No subject header is selected "(browser) {
+    
+    page
+      .click("@ContuctUs_Selector")//again t o submit another form,only first one doesn't need this
+      .waitForElementVisible("body")
+      .Set_Value("@emailSelector", "test@gmail.com")
+      .pause(3000)
+      .Set_Value("@messageSelector", "Test Case_4: failed to send this message,please select subject")
+      .pause(3000)
+      .click("@submitButtonSelector")
+      .pause(3000)
+      .assert.visible("@invalidsubject");
+  },
+
+  /*
+  my fifth test case: message is missing(it's required field should be provided)
+  */ 
+  "fifth test case: message is missing(it's required field should be provided)"(browser) {
+    
+    page
+      .click("@ContuctUs_Selector")//again t o submit another form,only first one doesn't need this
+      .selectFilter("@subjectHeadingDropDownSelector", WebMaster)
+      .pause(3000)
+      .waitForElementVisible("body")
+      .Set_Value("@emailSelector", "test@gmail.com")
+      .pause(3000)
+      .click("@submitButtonSelector")
+      .pause(3000)
+      .assert.visible("@invalidmessage");
+  },
+
+  /*
+  my sixth test case to prove that: only(message,subject header,email) are required fields,
+  while(reference order ,file attachement not required)->without them form will be submitted
+  */ 
+  "sixth test case:(reference order ,file attachement not required)->form will be submitted regarding their non-existance"(browser) {
+    
+    page
+      .click("@ContuctUs_Selector")//again t o submit another form,only first one doesn't need this
+      .selectFilter("@subjectHeadingDropDownSelector", CustomerService)
+      .pause(3000)
+      .waitForElementVisible("body")
+      .Set_Value("@emailSelector", "test@gmail.com")
+      .pause(3000)
+      .Set_Value("@messageSelector", "Test Case_6:(reference order ,file attachement not required fields")
+      .pause(3000)
+      .click("@submitButtonSelector")
+      .pause(3000)
+      .assert.visible("@successMessageSelector");
+  },
+      "My seventh test case to check wether there's format checking on email address entry,it should fail & messag: invalid email entry "(browser) {
+    
         page
           .click("@ContuctUs_Selector")//again t o submit another form,only first one doesn't need this
           .waitForElementVisible("body")
-          .Set_Value("@emailSelector", "test@gmail.com")
-          .pause(2000)
-          .Set_Value("@messageSelector", "Test Case_4: failed to send this message,please select subject")
-          .pause(2000)
+          .selectFilter("@subjectHeadingDropDownSelector", CustomerService)
+          .pause(3000)
+          .Set_Value("@emailSelector", "test")//wrong email format
+          .pause(3000)
+          .Set_Value("@messageSelector", "Test Case_7: wrong email format")
+          .pause(3000)
+          .Set_Value("@OrderReferenceInput",Order_num)
+          .pause(3000)
           .click("@submitButtonSelector")
           .pause(3000)
-          .assert.visible("@invalidsubject");
-      },
- 
-      /*
-      my fifth test case: message is missing(it's required field should be provided)
-      */ 
-      "fifth test case: message is missing(it's required field should be provided)"(browser) {
-        
-        page
-          .click("@ContuctUs_Selector")//again t o submit another form,only first one doesn't need this
-          .selectFilter("@subjectHeadingDropDownSelector", WebMaster)
-          .pause(2000)
-          .waitForElementVisible("body")
-          .Set_Value("@emailSelector", "test@gmail.com")
-          .pause(2000)
-          .click("@submitButtonSelector")
-          .pause(2000)
-          .assert.visible("@invalidmessage");
-      },
-
-      /*
-      my sixth test case to prove that: only(message,subject header,email) are required fields,
-      while(reference order ,file attachement not required)->without them form will be submitted
-      */ 
-      "sixth test case:(reference order ,file attachement not required)->form will be submitted regarding their non-existance"(browser) {
-        
-        page
-          .click("@ContuctUs_Selector")//again t o submit another form,only first one doesn't need this
-          .selectFilter("@subjectHeadingDropDownSelector", CustomerService)
-          .pause(2000)
-          .waitForElementVisible("body")
-          .Set_Value("@emailSelector", "test@gmail.com")
-          .pause(2000)
-          .Set_Value("@messageSelector", "Test Case_6:(reference order ,file attachement not required fields")
-          .pause(2000)
-          .click("@submitButtonSelector")
-          .pause(2000)
-          .assert.visible("@successMessageSelector");
+          .assert.visible("@invalidEmailAddress");
       }
+
   };
   
